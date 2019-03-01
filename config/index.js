cc0a14cc37e58f5a147c17724ac70b8017fe2c05
EsraaSaad298 <47531570+EsraaSaad298@users.noreply.github.com>
2019-03-01 16:25:02 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const getDbConnectionString = function () { // Set your connection string to mongoDB 
-    return "mongodb+srv://Esraa:<mimi45211!>@cluster0-bme9n.mongodb.net/test?retryWrites=true"
+    return "enter connection string here"
 }
 
 const getDevelopmentPort = function () {
