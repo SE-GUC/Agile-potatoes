f97d6a32d1608da847a3e23937a89f58239d88e2
Gasser Khaled <40330758+gskhaled@users.noreply.github.com>
2019-03-02 22:43:04 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const getDbConnectionString = function () { // Set your connection string to mongoDB 
-    return "mongodb+srv://gskhaled:12345@cluster0-wmsit.mongodb.net/test?retryWrites=true" ;
+    return "enter connection string here" ;
 }
 
 const getDevelopmentPort = function () {
