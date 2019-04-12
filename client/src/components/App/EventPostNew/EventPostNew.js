9b7980019777388f1658bc65ab5e6d78ac25aefe
IbrahimMohammed47 <ibrahim.mohammed@student.guc.edu.eg>
2019-04-12 19:40:43 +0200

    Added navbar component

--- client/src/components/App/EventPostNew/EventPostNew.js
+++ client/src/components/App/EventPostNew/EventPostNew.js
@@ -37,7 +37,7 @@ export class EventPostNew extends Component {
         }],
         commentsByPartner: [{
           text: 'Hello Admin',
-          date: new Date().toLocaleString(),
+          date: new Date().toLocaleDateString() ,
           author: {name: 'Seekurity'}
         }]
       }
@@ -45,7 +45,10 @@ export class EventPostNew extends Component {
   }
 
   async componentDidMount(){
+    const { match: { params } } = this.props;
+    console.log(params);
     // await get event post and put it in state
+
     let booked = await this.checkIfAlreadyBooked();
     if(booked){
       this.setState({
@@ -69,10 +72,10 @@ export class EventPostNew extends Component {
           <div className="event-post offset-sm-2 col-sm-8 row ">
             <div className="left-of-post col-sm-9">
               <div className="event-post-header">
-                <p className="text-muted"><i class="fas fa-calendar-day"></i>  {this.state.eventData.eventDate}</p>
+                <p className="text-muted"><i className="fas fa-calendar-day"></i>  {this.state.eventData.eventDate}</p>
                 <h2>{this.state.eventData.name}</h2>
                 <p><span className="text-muted">organized by </span>{this.state.eventData.partner.name}</p>
-                <p className="text-muted"><i class="fas fa-map-marker-alt"></i>  {this.state.eventData.location}, {this.state.eventData.city}</p>
+                <p className="text-muted"><i className="fas fa-map-marker-alt"></i>  {this.state.eventData.location}, {this.state.eventData.city}</p>
               </div>
               <div className="event-post-info">
                 <h4>Details</h4>
@@ -80,12 +83,12 @@ export class EventPostNew extends Component {
                 <br/>
                 <h4>Speakers</h4>
                 <div className="row">{this.state.eventData.speakers.map((speaker)=>{
-                  return <div className="col-sm-4"><i> -{speaker}</i></div>
+                  return <div key={speaker} className="col-sm-4"><i> -{speaker}</i></div>
                 })}</div>
                 <br/>
                 <h4>Topics</h4>
-                <div className="row">{this.state.eventData.topics.map((speaker)=>{
-                  return <div className="col-sm-4"><p> {speaker}</p></div>
+                <div className="row">{this.state.eventData.topics.map((topic)=>{
+                  return <div key={topic} className="col-sm-4"><p> {topic}</p></div>
                 })}</div>  
               </div>
               {
@@ -96,7 +99,7 @@ export class EventPostNew extends Component {
                 <div className="comments-section col-sm-12">
                   <h4>Comments</h4>
                   {this.getCommentsSorted().map((comment)=>{
-                    return <div className='comment'>
+                    return <div key={comment.date} className='comment'>
                       <p className="font-weight-bold">{comment.author.name}<span className="text-muted float-right font-weight-lighter">{comment.date}</span></p>
                       <p>{comment.text}</p>
                     </div>
@@ -115,9 +118,9 @@ export class EventPostNew extends Component {
             <div className="right-of-post col-sm-3">
               <p className="text-center h3">{this.state.eventData.price} EGP</p>
               {this.state.userHasBooked?(
-                <button class="btn btn-danger offset-sm-1 col-sm-10 book-button" disabled={this.state.eventData.remainingPlaces<=0} >Cancel</button>
+                <button className="btn btn-danger offset-sm-1 col-sm-10 book-button" disabled={this.state.eventData.remainingPlaces<=0} >Cancel</button>
               ):(
-                <button class="btn btn-outline-success offset-sm-1 col-sm-10 book-button" disabled={this.state.userData.userType!=="Member"||this.state.eventData.remainingPlaces<=0} >BOOK NOW</button>
+                <button className="btn btn-outline-success offset-sm-1 col-sm-10 book-button" disabled={this.state.userData.userType!=="Member"||this.state.eventData.remainingPlaces<=0} >BOOK NOW</button>
               )}
               <p className="text-muted text-center">remaining seats:{this.state.eventData.remainingPlaces}</p>
               {
@@ -126,7 +129,7 @@ export class EventPostNew extends Component {
                 (this.state.eventData.eventStatus === "Submitted")
                 &&
                 <div><br/><br/><br/>
-                  <button class="btn btn-success ctrl-button col-sm-12 ">Approve Event</button>
+                  <button className="btn btn-success ctrl-button col-sm-12 ">Approve Event</button>
                 </div>
               }
               {
@@ -135,7 +138,7 @@ export class EventPostNew extends Component {
                 (this.state.eventData.eventStatus === "Submitted")
                 &&
                 <div><br/><br/><br/>
-                  <button class="btn btn-danger ctrl-button col-sm-12 ">Delete Event</button>
+                  <button className="btn btn-danger ctrl-button col-sm-12 ">Delete Event</button>
                 </div>
               }
               {
@@ -144,7 +147,7 @@ export class EventPostNew extends Component {
                 (this.state.eventData.eventStatus === "Approved")
                 &&
                 <div><br/><br/><br/>
-                  <button class="btn btn-warning ctrl-button col-sm-12 ">Close Event</button>
+                  <button className="btn btn-warning ctrl-button col-sm-12 ">Close Event</button>
                 </div>
               }
               {
@@ -153,7 +156,7 @@ export class EventPostNew extends Component {
                 (this.state.eventData.eventStatus === "Finished")
                 &&
                 <div><br/><br/><br/>
-                  <button class="btn btn-success ctrl-button col-sm-12 ">Re-Open Event</button>
+                  <button className="btn btn-success ctrl-button col-sm-12 ">Re-Open Event</button>
                 </div>
               }
             </div>  
