c3b16db7ec321aac22755b7c3a5f821370471cc0
nadahesham <nadahesham456@yahoo.com>
2019-04-15 01:00:22 +0200

    Nada_Sprint5

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -10,14 +10,20 @@ import './App.css';
 // import Notifications from '../Notifications/Notifications';
 // import Header from '../Notifications/AppHeader';
 // import GetPendingEventsAdmin from './GetPendingEventsAdmin/GetPendingEventsAdmin'
-// import CreatingVacForm from './CreatingVacForm'
-// import GetPendingVacanciesAdmin from './GetPendingVacanciesAdmin'
+import CreatingVacForm from './CreatingVacForm'
+ import GetPendingVacanciesAdmin from './GetPendingVacanciesAdmin'
 import EventPostNew from './EventPostNew/EventPostNew'
+import CreatingVacComment from './CreatingVacComment'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EventPostNew/>
+            <EventPostNew />
+            <CreatingVacForm />
+            <GetPendingVacanciesAdmin />
+            <CreatingVacComment/>
+
+
         {/* <GetApplicants/>
         <SubmitFeedbackForm />
         <CreatePartnerform/>
