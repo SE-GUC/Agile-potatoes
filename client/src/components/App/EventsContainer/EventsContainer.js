fc979775ddac1bfe825753a06499ef4cbf46fe22
darknsSs512 <msapry007@gmail.com>
2019-05-04 15:40:45 +0200

    fixed some rendering issues in events and vacancies

--- client/src/components/App/EventsContainer/EventsContainer.js
+++ client/src/components/App/EventsContainer/EventsContainer.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import CreateEvent from '../CreateEvent/CreateEvent'
 class EventsContainer extends Component {
   state = {
+    create:false,
     events: [
       
     ],
@@ -16,7 +17,7 @@ class EventsContainer extends Component {
   getAllEvents = async ()=>{
     try {
       let allEvents = await axios.get(`http://localhost:3001/api/event/ApprovedEvents`);
-      this.setState({toggle:this.state.toggle ,events:[...allEvents.data]});
+      this.setState({toggle:0 ,events:[...allEvents.data]});
      } catch (error) {
       console.log('GOT ERROR')
     }
@@ -25,7 +26,7 @@ class EventsContainer extends Component {
     try {
       let tokenData = JSON.parse(localStorage.getItem('token')).data;
       let recommendEventsRes = await axios.get(`http://localhost:3001/api/event/RecommendedEvents`, { headers: { Authorization: 'Bearer '+ tokenData.authData }  });
-      this.setState({toggle:this.state.toggle ,events:[...recommendEventsRes.data]});
+      this.setState({toggle:0,events:[...recommendEventsRes.data]});
      } catch (error) {
       console.log('GOT ERROR')
     }
@@ -34,7 +35,7 @@ class EventsContainer extends Component {
     try {
       let tokenData = JSON.parse(localStorage.getItem('token')).data;
       let events = await axios.get(`http://localhost:3001/api/event/PartnerEvents`, { headers: { Authorization: 'Bearer '+ tokenData.authData } });
-      this.setState({toggle:this.state.toggle ,events:[...events.data]});
+      this.setState({toggle:0 ,events:[...events.data]});
      } catch (error) {
       console.log('GOT ERROR '+error)
     }
@@ -45,7 +46,7 @@ class EventsContainer extends Component {
     let tokenData = JSON.parse(localStorage.getItem('token')).data;
     let response = await axios.get('http://localhost:3001/api/event/PendingEventsAdmin', { headers: { Authorization: 'Bearer ' + tokenData.authData}
     })
-    this.setState({toggle:this.state.toggle ,events:[...response.data]});
+    this.setState({toggle:0 ,events:[...response.data]});
     } catch (error) {
       console.log('GOT ERROR ' + error)
     }
@@ -69,12 +70,49 @@ class EventsContainer extends Component {
     this.setState({events:[],toggle:1})
   }
   render() {
-    let func;
-    if(this.state.toggle === 1)
+    if(this.state.toggle === 0)
     {
-      func =  <CreateEvent/>
-      
+      return(
+        <div className='container-fluid'>
+        <div className='row'>
+            <div className='side-bar col-sm-2 ' >
+                <div className="list-group">
+                  <button type="button" onClick={this.getAllEvents} id="allEvents" className="list-group-item list-group-item-action">All Events</button>
+                  {JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Member' 
+                    &&
+                  <button type="button" onClick={this.getRecommendedEvents} id="recommendedEvents" className="list-group-item list-group-item-action">Recommended Events</button>
+                  }
+                  {JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Partner' 
+                    &&
+                  <button type="button" onClick={this.getPartnerEvents} id="partnerEvents" className="list-group-item list-group-item-action">Partner Events</button>
+                  }
+                  {
+                    JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Admin' 
+                    &&
+                  <button type="button" onClick={this.getPendingEventsAdmin} id="adminEvents" className="list-group-item list-group-item-action">Admin Pending Events</button>
+                  }
+                  {
+                    JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Member' 
+                    &&
+                  <button type="button" onClick={this.getMyAttendedEvents} id="memberEvents" className="list-group-item list-group-item-action">Your Events</button>
+                  }
+                  {
+                    (JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Partner' || JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Admin')
+                    &&
+                  <button type="button" onClick={this.handletoggle} id="partnerEvents" className="list-group-item list-group-item-action">Create Event</button>
+                  }
+                </div>
+            </div>
+            <div className='events-window col-sm-10'>
+            <EventsDisplayer events={this.state.events}/>
+            </div>
+        </div>
+      </div>
+
+      )
+       
     }
+    else      
     return (
       <div className='container-fluid'>
         <div className='row'>
@@ -107,8 +145,7 @@ class EventsContainer extends Component {
                 </div>
             </div>
             <div className='events-window col-sm-10'>
-              <EventsDisplayer events={this.state.events}/>
-              {func}
+            <CreateEvent/>
             </div>
         </div>
       </div>
--- client/src/components/App/VacanciesContainer/VacanciesContainer.js
+++ client/src/components/App/VacanciesContainer/VacanciesContainer.js
@@ -3,7 +3,7 @@ import './VacanciesContainer.css'
 import VacancyDisplayer from './VacancyDisplayer/VacancyDisplayer'
 import axios from 'axios'
 import CreateVacancy from '../CreateVacancy/CreateVacancy'
-class EventsContainer extends Component {
+class VacanciesContainer extends Component {
   state = {
     vacancies: [],
     toggle: 0,
@@ -13,18 +13,32 @@ class EventsContainer extends Component {
     try {
       let allVacancies = await axios.get(`http://localhost:3001/api/vacancy/getAllVacancies`);
       this.setState({
+        toggle:0,
         vacancies: allVacancies.data
       });
     } catch (error) {
       console.log('GOT ERROR fetching vacancies')
     }
   }
+  getAllVacancies =async ()=>{
+    try{
+    let allVacancies = await axios.get(`http://localhost:3001/api/vacancy/getAllVacancies`);
+    this.setState({
+      toggle:0,
+      vacancies: allVacancies.data
+    });
+  }
+  catch(error){
+    console.log('GOT ERROR fetching vacancies')
+  }
 
+  }
   getRecommendedVacancies = async () => {
     try {
       let tokenData = JSON.parse(localStorage.getItem('token')).data;
       let recommendVacanciesRes = await axios.get(`http://localhost:3001/api/vacancies/RecommendedVacancies`, { headers: { Authorization: 'Bearer ' + tokenData.authData } });
       this.setState({
+        toggle:0,
         vacancies: recommendVacanciesRes.data
       });
     } catch (error) {
@@ -37,6 +51,7 @@ class EventsContainer extends Component {
       let tokenData = JSON.parse(localStorage.getItem('token')).data;
       let vacancies = await axios.get(`http://localhost:3001/api/vacancy/PartnerVacancies`, { headers: { Authorization: 'Bearer ' + tokenData.authData } });
       this.setState({
+        toggle:0,
         vacancies: vacancies.data
       });
     } catch (error) {
@@ -50,6 +65,7 @@ class EventsContainer extends Component {
     let response = await axios.get('http://localhost:3001/api/vacancy/myPastVacancies', { headers: { Authorization: 'Bearer ' + tokenData.authData}
     })
     this.setState({
+      toggle:0,
       vacancies: response.data
     });
     } catch (error) {
@@ -80,7 +96,45 @@ class EventsContainer extends Component {
 
   render() {
     if (this.state.toggle > 0) {
-      return <CreateVacancy />
+      return(
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='side-bar col-sm-2 ' >
+            <div className="list-group">
+              <button type="button" onClick={this.getAllVacancies} id="allVacancies" className="list-group-item list-group-item-action">All Vacancies</button>
+              {
+                JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Member'
+                &&
+                <button type="button" onClick={this.getRecommendedVacancies} id="recommendedVacancies" className="list-group-item list-group-item-action">Recommended Vacancies</button>
+              }
+              {
+                JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Member'
+                &&
+                <button type="button" onClick={this.getMyPastVacancies} id="aWDFDGhvmbhjgfdstghjcv," className="list-group-item list-group-item-action">Your past Vacancies</button>
+              }
+              {
+                JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Partner'
+                &&
+                <button type="button" onClick={this.getPartnerVacancies} id="partnerVacancies" className="list-group-item list-group-item-action">All your Vacancies</button>
+              }
+              {
+                JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Admin'
+                &&
+                <button type="button" onClick={this.getPendingVacanciesAdmin} id="partnerVacancies" className="list-group-item list-group-item-action">Vacancies that need approval</button>
+              }
+              {
+                (JSON.parse(localStorage.getItem('token')).data.userData.userType === 'Partner')
+                &&
+                <button type="button" onClick={this.handletoggle} id="partnerVacancies" className="list-group-item list-group-item-action">Create Vacancy</button>
+              }
+            </div>
+          </div>
+          <div className='events-window col-sm-10'>
+          <CreateVacancy />
+          </div>
+        </div>
+      </div>
+      )
     }
     return (
       <div className='container-fluid'>
@@ -124,4 +178,4 @@ class EventsContainer extends Component {
   }
 }
 
-export default EventsContainer
+export default VacanciesContainer
